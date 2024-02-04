e115d5651d0a8b6793e2a6b3129e11cf06abc986
Farzaneh <farzanekazemi8517@gmail.com>
2024-02-04 21:47:37 +0330

    version 1.0.0

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,12 +1,24 @@
 import Header from "@/Components/modules/Header";
 import ProductsDetails from "@/Components/modules/Products/ProductDetails";
+import Testimonial from "@/Components/templates/Index/Testimonial";
 import React from "react";
 
-export default function Product({ product }) {
+export default function Product({ product, productComments }) {
+  console.log(productComments);
   return (
     <>
       <Header currentPage={product.title} prevPage="menu" />
       <ProductsDetails data={product} />
+
+      {productComments ? (
+        // productComments.slice(0,6).map(comment=>(
+        //   <div className="container">
+        <Testimonial comments={productComments} />
+      ) : (
+        // </div>
+        // ))
+        ""
+      )}
     </>
   );
 }
@@ -17,8 +29,8 @@ export async function getStaticPaths() {
 
   const paths = data.map((product) => ({
     params: {
-      id: String(product.id)
-    }
+      id: String(product.id),
+    },
   }));
 
   return {
@@ -33,9 +45,18 @@ export async function getStaticProps(context) {
   const res = await fetch(`http://localhost:4000/menu/${params.id}`);
   const product = await res.json();
 
+  const Comments = await fetch(`http://localhost:4000/comments`);
+  const AllComments = await Comments.json();
+
+  const productComments = AllComments.filter(
+    (data) => data.productID === +params.id
+  );
+
   return {
     props: {
       product,
+      productComments,
     },
+    revalidate: 60 * 60 * 12,
   };
 }
